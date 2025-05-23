c9537e52d6274ba0af9c55c1fd6a197ac883fcef
Jorge Aragon <jorge.aragon19@gmail.com>
2025-05-23 17:11:21 -0600

    Actualizacion formulario

--- js-07-api_fetch/src/pages/register/leerInputsFormulario.js
+++ js-07-api_fetch/src/pages/register/leerInputsFormulario.js
@@ -1,15 +1,14 @@
 
 
-const leerInputsFormulario = ( formularios ) => {
-
+const leerInputsFormulario = ( formulario )=>{
     const newUser = {
-        name : formularios.element["name"].value,
-        /* 
-        Leer el resto de valores
+        name : formulario.elements["name"].value,
+        /*
+         leer el resto de valores
         */
-       checkbox: formulario.elements["terminos-condiciones"].checked
+        checkbox: formulario.elements["terminos-condiciones"].checked
     }
+ 
+    return newUser;
 }
-    
-
-export {leerInputsFormulario};
+export { leerInputsFormulario };

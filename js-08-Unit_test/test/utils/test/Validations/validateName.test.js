a0d69d598e0bb3be71e6f8fc110a39fa63f44efe
Jorge Aragon <jorge.aragon19@gmail.com>
2025-05-29 17:44:11 -0600

    Unit test

---
+++ js-08-Unit_test/test/utils/test/Validations/validateName.test.js
@@ -0,0 +1,21 @@
+/**
+ *  TDD
+ *  TDD (Test-Driven Development) es una metodología de desarrollo 
+ *  de software donde las pruebas unitarias se escriben antes del 
+ *  código de producción.
+ * 
+ *  Ventajas de TDD: 
+ *  - Te obliga a pensar en los requerimientos antes de codificar.
+ *  - Mejora la calidad del código al asegurar que está cubierto por pruebas.
+ *  - Facilita la detección de errores rápidamente.
+ * 
+ */
+
+import { validateName } from "../../../../src/utils/Validations/validateName";
+
+// test ( "mensaje de la prueba" , ()=>{} );
+
+test ("Debe aceptar un nombre válido", ()=>{
+    // expect(valorRecibido.matcher(valorEsperado);
+    
+})
